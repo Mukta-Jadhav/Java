65075767cb3260d5b43056b95038553bfd65b4d2
Mukta-Jadhav <muktajadhav9284@gmail.com>
2025-09-26 10:18:51 +0530

    Add files via upload

---
+++ Jdbc/OracleConnection.java
@@ -0,0 +1,21 @@
+package Jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class OracleConnection {
+
+	public static void main(String[] args) throws Exception
+	{
+		  Class.forName("Oracle.jdbc.driver.OracleDriver");
+		  Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","System","123");          Statement st=con.createStatement();
+		  String Query="Select * from employee";
+		  ResultSet Es=st.executeQuery(Query);
+		  while(Es.next()) {
+			  System.out.println(Es.getInt(1)+" "+Es.getString(2));
+		  }
+	}
+
+}
